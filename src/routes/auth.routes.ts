855dfaa4bed14888a43c6971b3ed0632691d1501
vishnu <getvishnuvs@gmail.com>
2026-07-19 15:32:25 +0530

    add routes

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,11 +1,26 @@
-import express from "express";
-import { AuthMiddleware } from "../middlewares/auth.middleware";
+import { Router } from "express";
+import { loginSchema, registerSchema } from "../schemas/auth.schema";
+import { validate } from "../middlewares/validation.middleware";
 import { authController, authMiddleware } from "../di/auth.container";
-const router = express.Router();
 
-// route.get("/register",)
-router.get(
-    "/me",
-   authMiddleware.authenticate,
-    authController.me
+const router = Router();
+
+router.post(
+  "/register",
+  validate(registerSchema),
+  authController.register
 );
+
+router.post(
+  "/login",
+  validate(loginSchema),
+  authController.login
+);
+
+router.get(
+  "/me",
+  authMiddleware.authenticate,
+  authController.me
+);
+
+export default router;
